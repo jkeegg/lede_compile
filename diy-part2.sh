22d2d9e5a4ee5475246f0581c27d0fba0b813859
Chen <kvs112219@gmail.com>
2024-03-12 12:27:46 +0800

    fix go 1.22

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.250/g' package/base-files/files/bin/config_generate
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a
